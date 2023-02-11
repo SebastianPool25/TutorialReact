26a734d5a3a918cdb457e712ae0f2631cd43b4fc
SebastianPool25 <jose.19070054@itsmotul.edu.mx>
2023-02-11 15:24:44 -0600

    Para poder conectar el board con la función OnSquareClick se usa handleClick para detectar un click

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import { useState } from 'react';
 
-function Square({value}) {
-  return <button className="square">{value}</button>;
+function Square({ value, onSquareClick }) {
+  return (
+    <button className="square" onClick={onSquareClick}>
+      {value}
+    </button>
+  );
 }
 
 function handleClick() {
@@ -23,7 +27,7 @@ export default function Board() {
   return (
     <>
       <div className="board-row">
-        <Square value={squares[0]} />
+      <Square value={squares[0]} onSquareClick={handleClick} />
         <Square value={squares[1]} />
         <Square value={squares[2]} />
       </div>
