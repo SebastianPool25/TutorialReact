d684587a20eaeb48ce7f6d7aa959e09219d54c4b
SebastianPool25 <jose.19070054@itsmotul.edu.mx>
2023-02-11 19:16:10 -0600

    juego terminado vamos a jugar gato

--- src/App.js
+++ src/App.js
@@ -53,21 +53,19 @@ function Board({ xIsNext, squares, onPlay }) {
 }
 
 export default function Game() {
-  const [xIsNext, setXIsNext] = useState(true);
   const [history, setHistory] = useState([Array(9).fill(null)]);
   const [currentMove, setCurrentMove] = useState(0);
+  const xIsNext = currentMove % 2 === 0;
   const currentSquares = history[currentMove];
 
   function handlePlay(nextSquares) {
-  const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
-  setHistory(nextHistory);
-  setCurrentMove(nextHistory.length - 1);
-    setXIsNext(!xIsNext);
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length - 1);
   }
 
   function jumpTo(nextMove) {
     setCurrentMove(nextMove);
-    setXIsNext(nextMove % 2 === 0);
   }
 
   const moves = history.map((squares, move) => {
@@ -96,7 +94,7 @@ export default function Game() {
   );
 }
 
-function calculateWinner(squares) { 
+function calculateWinner(squares) {
   const lines = [
     [0, 1, 2],
     [3, 4, 5],
