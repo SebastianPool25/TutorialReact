23ea1a8648f787db0f2b35de56ea20d7f73274ad
Iverson G.M <iverson.19070044@itsmotul.edu.mx>
2023-02-12 22:33:47 -0600

    Comentarios del codigo - Iverson Mex

--- src/App.js
+++ src/App.js
@@ -1,19 +1,27 @@
+/*Comentarios Iverson Gabriel Mex Pech*/
+
+/* Libreria de react que permite "recordarle" al cuadrado el estado en el que esta*/
 import { useState } from 'react';
 
+/*Se crea componente que sirve para imprimir los recuadros del tablero
+Tiene un accesorio que sirve para detectar si el recuadro esta vacio o existe algo dentro de ella*/
 function Square({ value, onSquareClick }) {
   return (
+    /*Botón que detecta si se le hace click encima a un cuadro*/
     <button className="square" onClick={onSquareClick}>
       {value}
     </button>
   );
 }
-
+/*Función que crea el tablero de juego*/
 function Board({ xIsNext, squares, onPlay }) {
+  /*Función que define el orden en el que apareceran las (X o O)*/
   function handleClick(i) {
     if (calculateWinner(squares) || squares[i]) {
       return;
     }
     const nextSquares = squares.slice();
+    /*Condicion que controla que va a aparecer en el tablero*/
     if (xIsNext) {
       nextSquares[i] = 'X';
     } else {
@@ -22,6 +30,7 @@ function Board({ xIsNext, squares, onPlay }) {
     onPlay(nextSquares);
   }
 
+  /*Función que define al ganador del juego en base si hay mas turnos por jugar*/
   const winner = calculateWinner(squares);
   let status;
   if (winner) {
@@ -30,6 +39,7 @@ function Board({ xIsNext, squares, onPlay }) {
     status = 'Next player: ' + (xIsNext ? 'X' : 'O');
   }
 
+  /*Se agrupan los recuadros en filas de 3, y dependiendo lo que este en la función valor determina si hay (X o O) o no hay nada*/
   return (
     <>
       <div className="status">{status}</div>
@@ -52,18 +62,24 @@ function Board({ xIsNext, squares, onPlay }) {
   );
 }
 
+/*Función (Game )accesible desde los otros archivos que a su vez determina el valor que tendran los recuadros*/
 export default function Game() {
+
   const [history, setHistory] = useState([Array(9).fill(null)]);
+  /*Guarda en una variable el estado del tablero*/
   const [currentMove, setCurrentMove] = useState(0);
+  /*Variable que le da funcionalidad a los botones de avanzar y reiniciar el juego*/
   const xIsNext = currentMove % 2 === 0;
+  /*Muestra en el tablero el movimiento actual*/
   const currentSquares = history[currentMove];
 
+  /*Función que permite seguir el juego a partir de los movimientos anteriores*/
   function handlePlay(nextSquares) {
     const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
     setHistory(nextHistory);
     setCurrentMove(nextHistory.length - 1);
   }
-
+  /*Función que permite asignar el siguiente movimiento*/
   function jumpTo(nextMove) {
     setCurrentMove(nextMove);
   }
@@ -82,6 +98,7 @@ export default function Game() {
     );
   });
 
+  /*Función para mostrar los movimientos pasados*/
   return (
     <div className="game">
       <div className="game-board">
@@ -94,6 +111,7 @@ export default function Game() {
   );
 }
 
+/*Funcion que determina al ganador a traves de una matriz*/
 function calculateWinner(squares) {
   const lines = [
     [0, 1, 2],
--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
-import React, { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./styles.css";
+/*Comentarios Iverson Gabriel Mex Pech*/
 
+/*Index de importaciones*/
+
+/*Importamos React*/
+import React, { StrictMode } from "react";
+/*Importamos la libreria para que React pueda comunicarse con el navegador*/
+import { createRoot } from "react-dom/client";
+/*Importamos la hoja de estilos del proyecto*/
+import "./styles.css";
+/*Usamos esta importación para relacionar el archivo App.js con el archivo index.js */
 import App from "./App";
 
+/*Creamos una constante para renderizar el proyecto*/
 const root = createRoot(document.getElementById("root"));
 root.render(
   <StrictMode>
